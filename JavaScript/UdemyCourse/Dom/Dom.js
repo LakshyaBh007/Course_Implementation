be4e83568b9776689606b20bc0e43ddad06f6cbf
Lakshya Bhatia <lakshya.bhatia@argusoft.in>
2024-08-29 11:11:57 +0530

    update

--- JavaScript/UdemyCourse/Dom/Dom.js
+++ JavaScript/UdemyCourse/Dom/Dom.js
@@ -8,6 +8,10 @@
 
 //querySelector - can be used to select all of the above
 // on single element (first match)
+document.querySelector('h1'); //finds first h1 element
+document.querySelector('#red'); //finds element with id of red
+document.querySelector('.big') //finds element with class of
+
 //querySelectorAll - used to select the collection
 
 const h1 = document.querySelector('h1');
@@ -39,3 +43,38 @@ h1.innerHTML += ' is cool'
 h1.innerHTML += '<b>!!!!!</b>'//cant do with innerText as it will
 // add string not tag
 console.log(h1.innerText);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs); //value used for extracting input value
+
+console.log(inputs[2].value);
+console.log(inputs[2].checked);
+
+inputs[0].value = 'the bear';
+
+console.log(inputs[3].value);
+
+console.log(inputs[1].placeholder);
+inputs[1].placeholder = 'please enter password';
+
+const a = document.querySelector('a');
+console.log(a.href);
+a.href = "http://www.google.com";
+
+const img = document.querySelector('img');
+console.log(img.src);
+img.src = "https://upload.wikimedia.org/wikipedia/en/3/3c/Chris_Hemsworth_as_Thor.jpg"
+
+const range = document.querySelector('input[type="range"]');
+console.log(range.getAttribute('max'));
+console.log(range.getAttribute('min'));
+console.log(range.getAttribute('total')); //getting attributes
+console.log(range.getAttribute('type'));
+
+range.setAttribute('min','-500')
+range.setAttribute('type', 'radio') //setting attributes
+
+const firstLI = document.querySelector('li');
+
+
+
